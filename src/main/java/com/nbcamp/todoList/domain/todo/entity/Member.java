183b9a0e9eb08f106ca7e7ae98c8559531cc5d18
hubaek <zjfl3122@naver.com>
2024-10-29 17:25:05 +0900

    Feat : Member Entity 추가

---
+++ src/main/java/com/nbcamp/todoList/domain/todo/entity/Member.java
@@ -0,0 +1,31 @@
+package com.nbcamp.todoList.domain.todo.entity;
+
+import com.nbcamp.todoList.common.entity.Timestamped;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    public void update(String name) {
+        this.name = name;
+    }
+
+    public Member(String name) {
+        this.name = name;
+    }
+}
