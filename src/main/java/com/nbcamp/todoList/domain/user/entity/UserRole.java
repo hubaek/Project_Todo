c4ec2750f89b14ee66bed1727e7148d2398eabaa
hubaek <zjfl3122@naver.com>
2024-12-17 19:57:32 +0900

    Feat : UserRole 생성자 추가

--- src/main/java/com/nbcamp/todoList/domain/user/entity/UserRole.java
+++ src/main/java/com/nbcamp/todoList/domain/user/entity/UserRole.java
@@ -1,5 +1,7 @@
 package com.nbcamp.todoList.domain.user.entity;
 
+import java.util.Arrays;
+
 public enum UserRole {
     USER(Authority.USER),  // 사용자 권한
     ADMIN(Authority.ADMIN);  // 관리자 권한
@@ -8,6 +10,13 @@ public enum UserRole {
 
     UserRole(String authority) {
         this.authority = authority;
+    }
+
+    public static UserRole of(String userRole) {
+        return Arrays.stream(UserRole.values())
+                .filter(r -> r.name().equalsIgnoreCase(userRole))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user role: " + userRole));
     }
 
     public String getAuthority() {
